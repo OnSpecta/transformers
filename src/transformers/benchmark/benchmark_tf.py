c322a2ec9c305e32ce3f1bc040d4be680aae0ce2
MarcelWilnicki <marcel@onspecta.com>
2021-05-26 01:16:50 +0200

    update profiler

--- src/transformers/benchmark/benchmark_tf.py
+++ src/transformers/benchmark/benchmark_tf.py
@@ -53,20 +53,6 @@ logger = logging.get_logger(__name__)
 def run_with_tf_optimizations(do_eager_mode: bool, use_xla: bool):
 
     def run_func(func):
-
-        # # Set up logging.
-        # stamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-        # logdir = '/home/marcel/dev/logs/transformers_logs/%s' % stamp
-        # writer = tf.summary.create_file_writer(logdir)
-        #
-        # tf.summary.trace_on(graph=True, profiler=True)
-        # ## function call
-        # with writer.as_default():
-        #     tf.summary.trace_export(
-        #         name="my_func_trace",
-        #         step=0,
-        #         profiler_outdir=logdir)
-
         @wraps(func)
         def run_in_eager_mode(*args, **kwargs):
             return func(*args, **kwargs)
@@ -110,9 +96,23 @@ class TensorFlowBenchmark(Benchmark):
         strategy = self.args.strategy
         print('inside 2: 2')
         assert strategy is not None, "A device strategy has to be initialized before using TensorFlow."
+        # # Set up logging.
+        stamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+        logdir = '/onspecta/dev/logs/transformers/%s' % stamp
+        writer = tf.summary.create_file_writer(logdir)
+
+
         _inference = self._prepare_inference_func(model_name, batch_size, sequence_length)
         print('inside 2: 3')
-        return self._measure_speed(_inference)
+
+        tf.summary.trace_on(graph=True, profiler=True)
+        test = self._measure_speed(_inference)
+        with writer.as_default():
+            tf.summary.trace_export(
+                name="my_func_trace",
+                step=0,
+                profiler_outdir=logdir)
+        return test
 
     def _train_speed(self, model_name: str, batch_size: int, sequence_length: int) -> float:
         strategy = self.args.strategy
@@ -157,48 +157,59 @@ class TensorFlowBenchmark(Benchmark):
         if not self.args.only_pretrain_model and has_model_class_in_config:
             try:
                 print('inside 3: 3')
+                print('he he he')
                 model_class = "TF" + config.architectures[0]  # prepend 'TF' for tensorflow model
+                print('inside 3: 4')
                 transformers_module = __import__("transformers", fromlist=[model_class])
+                print('inside 3: 5')
                 model_cls = getattr(transformers_module, model_class)
+                print('inside 3: 6')
 
-                stamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-                logdir = '/onspecta/dev/logs/transformers_logs/%s' % stamp
-                writer = tf.summary.create_file_writer(logdir)
-
-                tf.summary.trace_on(graph=True, profiler=True)
+                print('is this tf????:')
                 model = model_cls(config)
-                with writer.as_default():
-                    tf.summary.trace_export(
-                        name="my_func_trace",
-                        step=0,
-                        profiler_outdir=logdir)
+                print('yesss')
             except ImportError:
                 raise ImportError(
                     f"{model_class} does not exist. If you just want to test the pretrained model, you might want to set `--only_pretrain_model` or `args.only_pretrain_model=True`."
                 )
         else:
-            print('inside 3: 4')
+            # print('inside 3: 4')
             model = TF_MODEL_MAPPING[config.__class__](config)
 
-        print('inside 3: 5')
+        # print('inside 3: 5')
         # encoder-decoder has vocab size saved differently
         vocab_size = config.vocab_size if hasattr(config, "vocab_size") else config.encoder.vocab_size
         input_ids = random_input_ids(batch_size, sequence_length, vocab_size)
 
+        # tf.summary.trace_on(graph=True, profiler=True)
+
+        # # Set up logging.
+        # stamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+        # logdir = '/onspecta/dev/logs/transformers/%s' % stamp
+        # writer = tf.summary.create_file_writer(logdir)
+
+        # tf.summary.trace_on(graph=True, profiler=True)
+        # with writer.as_default():
+        #     tf.summary.trace_export(
+        #         name="my_func_trace",
+        #         step=0,
+        #         profiler_outdir=logdir)
+
         @run_with_tf_optimizations(self.args.eager_mode, self.args.use_xla)
         def encoder_decoder_forward():
-            print('1tf?')
+            print('RUN_WITH_TF_OPTIMIZATIONS_1')
             return model(input_ids, decoder_input_ids=input_ids, training=False)
 
         # encoder_forward = run_with_tf_optimizations(encoder_forward)
         @run_with_tf_optimizations(False, self.args.use_xla)
         def encoder_forward():
-            print('2tf?')
-            return model(input_ids, training=False)
+            print('RUN_WITH_TF_OPTIMIZATIONS_2')
+            test = model(input_ids, training=False)
+            return test
 
         _inference = encoder_decoder_forward if config.is_encoder_decoder else encoder_forward
 
-        print('inside 3: 6')
+        # print('inside 3: 6')
         return _inference
 
     def _prepare_train_func(self, model_name: str, batch_size: int, sequence_length: int) -> Callable[[], None]:
--- src/transformers/benchmark/onspecta_benchmark.py
+++ src/transformers/benchmark/onspecta_benchmark.py
@@ -6,19 +6,10 @@ from datetime import datetime
 args = TensorFlowBenchmarkArguments(models=["bert-base-uncased"], batch_sizes=[8], sequence_lengths=[8])
 
 benchmark = TensorFlowBenchmark(args)
-
-# Set up logging.
-# stamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-# logdir = '/onspecta/dev/logs/transformers_logs/%s' % stamp
-# writer = tf.summary.create_file_writer(logdir)
-
-# tf.summary.trace_on(graph=True, profiler=True)
 results = benchmark.run()
-# with writer.as_default():
-#     tf.summary.trace_export(
-#         name="my_func_trace",
-#         step=0,
-#         profiler_outdir=logdir)
+
+
+#test12
 
 print(results)
 
